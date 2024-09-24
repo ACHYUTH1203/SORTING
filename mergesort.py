d82f14319d61938704bba26791f289570ce7a742
ACHYUTH1203 <166857838+ACHYUTH1203@users.noreply.github.com>
2024-09-24 18:54:54 +0530

    Create mergesort.py

---
+++ mergesort.py
@@ -0,0 +1,37 @@
+arr=[33,112,43,56,765]
+def mergesort(arr,low,high):
+    if (low>=high):
+        return
+    else :
+        mid=(low+high)//2
+        
+        mergesort(arr,low,mid)
+        mergesort(arr,mid+1,high)
+        
+        merge(arr,low,mid,high)
+def merge(arr,low,mid,high):
+    
+    temp=[]
+    
+    left = low
+    right= mid+1
+    
+    while (left<= mid) and (right<=high):
+        if (arr[left]<arr[right]):
+            temp.append(arr[left])
+            left+=1
+        else:
+            temp.append(arr[right])
+            right+=1
+    
+    while (left<=mid):
+        temp.append(arr[left])
+        left+=1
+    while (right<=high):
+        temp.append(arr[right])
+        right+=right
+    for i in range(len(temp)):
+        arr[low + i] = temp[i]
+
+mergesort(arr,0,len(arr)-1)
+print(arr)
